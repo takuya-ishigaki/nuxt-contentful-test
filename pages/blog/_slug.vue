a3110e9b0c71920975bde815b9a8f7f57f43782a
takuya ishigaki <takuya.ishigaki@i-studio.co.jp>
2020-04-30 20:11:51 +0900

    feat: body出力

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="container">
     <h1>{{ blog.fields.title }}</h1>
+    <div v-html="toHtmlString(blog.fields.body)"></div>
   </div>
 </template>
 
 <script>
+import { BLOCKS } from '@contentful/rich-text-types'
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer'
 import contentfulClient from '~/plugins/contentful.js'
 
 export default {
-  async asyncData ({ route }) {
+  async asyncData ({ params }) {
     const getPosts = async (contentType) => {
       const posts = await contentfulClient.getEntries({
         content_type: contentType,
-        'fields.slug': route.params.id,
-        limit: 10,
-        order: 'sys.createdAt'
+        'fields.slug': params.slug
       })
 
       return posts.items[0]
@@ -24,10 +25,22 @@ export default {
       blog: await getPosts('blog')
     }
   },
-  mounted () {
-    console.log(this.blog)
+  methods: {
+    toHtmlString (obj) {
+      const options = {
+        renderNode: {
+          [BLOCKS.EMBEDDED_ASSET]: ({ data: { target: { fields } } }) =>
+            `<img src="${fields.file.url}"/>`
+        }
+      }
+      return documentToHtmlString(obj, options)
+    }
   }
 }
 </script>
 
-<style></style>
+<style scoped>
+/deep/ img {
+  max-width: 100%;
+}
+</style>
